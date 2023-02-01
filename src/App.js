29d887ddc3c8ad7654e8811c20ed7ea845edb426
Fernando <21fernandinhow@gmail.com>
2023-02-01 18:32:30 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+  }
+
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition((position)=> {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    });
+  }, []);
+
+  if(location === false){
+
+    return (
+
+      <section id="main">
+        <h1>Você precisa habilitar a localização no seu browser.</h1>
+      </section>
+
+    );
+
+  } else if (weather === false) {
+    
+    return (
+
+      <section id="main">
+        <h1>Carregando o clima...</h1>
+      </section>   
+      
+    );
+
+  } else {
+
+    let title = weather['weather'][0]['description'];
+    return (
+    
+      <Fragment>
+
+        <section id="main">
+          <p>Clima em sua localização:</p>
+          <h1>{title[0].toUpperCase() + title.substring(1)} </h1>
+          <hr />
+          <h4>Mais informações:</h4>
+          <ul>
+            <li>Temperatura atual: {weather['main']['temp']}°</li>
+            <li>Temperatura máxima: {weather['main']['temp_max']}°</li>
+            <li>Temperatura minima: {weather['main']['temp_min']}°</li>
+            <li>Umidade: {weather['main']['humidity']}%</li>
+          </ul>
+        </section>
+
+        <footer>
+          <a href='https://fernandocarvalhodev.com'>Desenvolvido por Fernando Carvalho</a>
+        </footer>
+
+      </Fragment>
+      
+    );
+
+  };
+
+};
 
 export default App;
